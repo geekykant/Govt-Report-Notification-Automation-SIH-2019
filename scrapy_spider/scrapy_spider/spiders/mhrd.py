ebc81516644eae6796d3f3bad0385cb585230633
Sreekant <geekykant@gmail.com>
2019-02-24 00:47:56 +0530

    something working

--- scrapy_spider/scrapy_spider/spiders/mhrd.py
+++ scrapy_spider/scrapy_spider/spiders/mhrd.py
@@ -6,6 +6,8 @@ new_links = [];
 visited_links = [];
 pdf_links = []
 
+domain = urlparse('http://mhrd.gov.in/').netloc
+
 
 class MhrdSpider(scrapy.Spider):
     name = 'mhrd'
@@ -13,8 +15,9 @@ class MhrdSpider(scrapy.Spider):
     start_urls = ['http://mhrd.gov.in/']
 
     def parse(self, response):
+
         item_links = response.css('a::attr(href)').extract()
-        # print(response.url + " : " + str(len(item_links)))
+        print(response.url + " : " + str(len(item_links)))
 
         not_url = ['/', '#', 'javascript:void(0)']
         file_types = ['pdf', 'docx', 'docx']
@@ -22,15 +25,14 @@ class MhrdSpider(scrapy.Spider):
         def isDomainOnlyURL(link):
             link_p = urlparse(link)
             response_p = urlparse(response.url)
-            # print(link_p.path)
-            if (link_p.netloc == response_p.netloc and link_p.path != "/"):
-                return True
+            if (link_p.netloc == domain and link_p.path != "/"):
                 # print(link)
+                return True
 
         # href with '/#about' to convert to full href link
         def convertURL(my_url):
             if (my_url not in not_url):
-                if (my_url[:4] != "http"):
+                if (my_url[:5] != urlparse(response.url).scheme):
                     my_url = urljoin(response.url, my_url)
 
                 # Adding all links to array
@@ -39,27 +41,68 @@ class MhrdSpider(scrapy.Spider):
                         # Adding PDF's links
                         if (file_type in my_url and my_url not in pdf_links):
                             pdf_links.append(my_url)
-                            # print(my_url)
                         # Appending new follow links
                         elif (my_url not in new_links and my_url not in pdf_links):
                             new_links.append(my_url)
 
-                # print(my_url)
+                        # print(my_url)
 
         # Loop through the entire links
         for link in item_links:
             convertURL(link)
 
-        print(new_links)
+        # print(new_links)
 
-        # if new_links[i]:
-        #     print(new_links[i])
-        #     # absolute_next_page_url = response.urljoin(next_page_url)
-        #     yield scrapy.Request(new_links[i])
+        if (response.url in new_links):
+            new_links.remove()
+        visited_links.append(response.url)
+
+        for url in new_links:
+            if (url not in visited_links):
+                # We make a request to each url and call the parse function on the http response.
+                yield scrapy.Request(url=url, callback=self.parse_item)
 
     def parse_item(self, response):
-        print('Processing..' + response.url)
-        # print(response.text)
+        item_links = response.css('a::attr(href)').extract()
+        print(response.url + " : " + str(len(item_links)))
+
+        not_url = ['/', '#', 'javascript:void(0)']
+        file_types = ['pdf', 'docx', 'docx']
+
+        def isDomainOnlyURL(link):
+            link_p = urlparse(link)
+            response_p = urlparse(response.url)
+            if (link_p.netloc == domain and link_p.path != "/"):
+                # print(link)
+                return True
+
+        # href with '/#about' to convert to full href link
+        def convertURL(my_url):
+            if (my_url not in not_url):
+                if (my_url[:5] != urlparse(response.url).scheme):
+                    my_url = urljoin(response.url, my_url)
+
+                # Adding all links to array
+                for file_type in file_types:
+                    if (isDomainOnlyURL(my_url)):
+                        # Adding PDF's links
+                        if (file_type in my_url and my_url not in pdf_links):
+                            pdf_links.append(my_url)
+                        # Appending new follow links
+                        elif (my_url not in new_links and my_url not in pdf_links):
+                            new_links.append(my_url)
+
+                        # print(my_url)
+
+        if (response.url in new_links):
+            new_links.remove()
+        visited_links.append(response.url)
+
+        # Loop through the entire links
+        for link in item_links:
+            convertURL(link)
+
+        # print(new_links)
 
 
 print("Hello!!")
