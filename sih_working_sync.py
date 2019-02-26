93a8badeb14b3845499156021c94e87b9d5ce91c
Sreekant <geekykant@gmail.com>
2019-02-26 21:59:55 +0530

    new files

--- sih_working_sync.py
+++ sih_working_sync.py
@@ -2,46 +2,72 @@ from urllib.parse import urlparse, urljoin
 import requests
 
 import lxml.html
+import sqlite3
+
+import pathlib
 
 new_links = [];
 visited_links = [];
 pdf_links = []
 
 not_url = ['/', '#', 'javascript:void(0)']
-file_types = ['.pdf', '.doc', '.docx', '.xlxs','.ppt', '.pptx','.jpg', '.png']
+file_types = ['.pdf', '.doc', '.docx', '.xlxs', '.PDF']
+not_file_types = ['.mp3', '.jpg', '.png', '.ppt', '.pptx', '.JPG']
 
 # main_url = 'http://www.paavam.com'
 url = 'http://mhrd.gov.in/'
 domain = urlparse(url).netloc
 
+here = pathlib.Path(__file__).parent
+outpath = here.joinpath("govt_db.db")
+
+conn = sqlite3.connect(outpath)
+c = conn.cursor()
+
+# Create table
+# c.execute("CREATE TABLE {url} (id INTEGER PRIMARY KEY AUTO_INCREMENT, sub_url TEXT, file_name TEXT);".format(url=domain.replace('.','_')))
+c.execute("CREATE TABLE {url} (sub_url TEXT, file_name TEXT);".format(url=domain.replace('.', '_')))
+
+
 def scrapeIt(new_url):
     content = requests.get(new_url).content
+
     def extractLinks(content):
         dom = lxml.html.fromstring(content)
         # print("" + new_url + " : " + str(len(dom.xpath('//a/@href'))))
         for link in dom.xpath('//a/@href'):
-
             # retun if '#' and links
-            if(link not in not_url):
+            if (link not in not_url):
                 # convert '/gallery' to full link "https://../gallery"
                 if (urlparse(link).scheme != urlparse(new_url).scheme):
                     link = urljoin(new_url, link)
 
                 # check for same domain links
-                if(urlparse(link).netloc == domain):
-                    #removing link parameters
-                    if('#' in link or '?' in link or '&' in link):      
-                            link = urljoin(url, urlparse(link).path)
-                            
-                    for file_type in file_types:
-                            if(file_type in link and link not in pdf_links):
-                                pdf_links.append(link)
+                if (urlparse(link).netloc == domain):
+                    # removing link parameters
+                    if ('#' in link or '?' in link or '&' in link):
+                        link = urljoin(url, urlparse(link).path)
 
-                    if(link not in new_links and link not in pdf_links):
-                        new_links.append(link)
-                    
+                    for file_type in file_types:
+                        if (file_type in link and link not in pdf_links):
+                            if (requests.get(link).status_code != 404):
+                                pdf_links.append(link)
+                                # Insert a row of data and commit
+                                # c.execute("INSERT INTO " + url + " (sub_url, file_name) VALUES ('http://www.google.com','aaa');")
+                                c.execute("INSERT INTO " + domain.replace('.', '_') + " VALUES (?,?)", (link, 'aaa'))
+                                conn.commit()
+
+                    if (link not in new_links and link not in pdf_links):
+                        flag = 1
+                        for not_file_type in not_file_types:
+                            if (not_file_type in link):
+                                flag = 0
+
+                        if (flag):
+                            new_links.append(link)
 
     extractLinks(content)
+
 
 # First scrape
 scrapeIt(url)
@@ -51,4 +77,5 @@ for i in new_links:
     print(i + " : " + str(len(new_links)))
     scrapeIt(i)
 
-print("completed!")    
+print("completed!")
+conn.close()
